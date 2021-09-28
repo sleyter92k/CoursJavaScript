a95fdbc5a40560f42020ede1f702c88f22f05571
Sleyter Monteiro <sleytermonteiro92@protonmail.com>
2021-09-28 16:59:48 +0200

    intro suite

---
+++ 02_variable_constante/affichage.js
@@ -0,0 +1,18 @@
+// js pour affichage
+
+let ville = 'Nice';
+console.log(ville);
+
+let prixLogement = 1200;
+console.log(prixLogement);
+
+alert(prixLogement);
+
+// mini-exo ; demandez à un utilisateur
+
+let question = prompt('Dans quelle ville êtes-vous né(e) ?');
+        alert('Vous êtes né dans la ville: '+ question.toLocaleUpperCase());
+        document.write(question + question.length);
+
+let villeDeNé = prompt('Dans quelle ville avez-vous né ?');
+        document.write(villeDeNé.toLocaleUpperCase());
---
+++ 02_variable_constante/introduction.js
@@ -0,0 +1,49 @@
+// mon second fichier js externe
+
+// création de 3 variables qui ne stockent pas de valeurs
+
+let prenom;
+let monAge;
+
+var ville;
+
+// le signe égal = est un opérateur d'affectation
+prenom = 'Jean'; // cette variable contient une chaine de caractères
+
+console.log(prenom);
+
+let monAge = 40 // cette variable contient un nombre
+
+// Ceci fonctionne avec var on initialise la variable après affectation
+nom = 'yourcenar';
+var nom;
+console.log(nom);
+
+// ceci fonctionne pas avec let il faut initialiser la variable avant affectation;
+// vehicule = autobus;
+// let vehicule;
+// console.log(vehicule);
+
+var chien = 'Médor';
+console.log (chien);
+var chien = 'KiKi';
+console.log (chien);
+
+// ceci ne fonctionne pas
+// var chien = 'Médor';
+// console.log (chien);
+// var chien = 'KiKi';
+
+if (true){
+    let animal = 'Tigre';
+    console.log(animal);
+}
+
+// console.log (animal
+
+if (true){
+    var objet ='lampe';
+    console.log(objet);
+}
+console.log(objet);
+
