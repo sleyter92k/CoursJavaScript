746135a552db2babe3ec9d4d2d20aa00865fee6a
Sleyter Monteiro <sleyter.monteiro@colombbus.org>
2021-10-05 11:02:23 +0200

    le 05/10/2021

---
+++ 02_variable_constante/concatenation.js
@@ -0,0 +1,45 @@
+// Script pour la page concaténation
+
+// alert('coucou !')
+
+// une variable dans laquelle on met notre age avec un calcul
+
+let monAge = 18 + 2;
+console.log(monAge);
+
+// utilisation du caractère d'échappement
+let phrase = 'bonjour j\'ai';
+
+// let phrase2 = "Bonjour j'ai faim"
+
+//  let test = '4 + 5';
+
+console.log(phrase);
+
+console.log(monAge, phrase);
+
+// MINI EXO faire une alerte et mettre dans l'alerte "Bonjour j'ai 20 ans"
+
+alert(phrase + ' ' + monAge + ' ans.');
+
+let x = 501;
+let y = 99;
+
+// MINI EXO écrire, dans une alerte "x contient 501, y contient 99 : leur somme est égal à 600"
+
+// console.log(x,y);
+
+
+// console.log(x, y);
+// console.log(a)
+// alert('x contient 501, y contient 99');
+
+// console.log('x contient' + x + ' y contient ' + y + ' leur somme est égale à ' + (x+y));
+
+// alert(`x contient ${x}`);
+
+// alert(`y contient ${y}`);
+
+// alert(`x contient ${x}`);
+
+alert(`x contient ${x} y contient ${y} leur somme est égale à{x+y}`);
