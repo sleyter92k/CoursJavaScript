2cf98b8c9b4cba329b9d1c40da6cef661dc04c11
Sleyter Monteiro <sleytermonteiro92@protonmail.com>
2021-10-29 19:32:47 +0200

    parite

--- 00_exos/parite.js
+++ 00_exos/parite.js
@@ -1,24 +1,36 @@
-let i =prompt('saisez un nombre');
+console.log('*********** DEMO FOR ********');
 
-function impair(i) {
-        return i % 2
+for (var i = 1; i<=10; i++) {
+    if (i % 2 === 0) {
+       console.log(i + ' est pair.');
     }
-    console.log(i)
-
-if (i % 2 == 0){
-    alert('pair')
-    console.log (i  + ' est pair ');
-} else {
-    alert('impair')
-    console.log (i  + ' est impair ');
 }
-
-for ( let  i = 0; i <= 20 ; i++ ) {
-    if ( i % 2 == 0) {
-    console.log ( i +  ' est pair ');
-    } else if(i % 2 !==0) {
-            console.log ( i +  ' est impair ');
+// exo 1
+console.log('********* EXO 1 **********'); 
+for (var i = 1; i<=10; i++) {
+    if (i % 2 === 0) {
+       console.log(i + ' est pair.');
     } else {
-        document.querySelector
+        console.log(i + ' est impair');
     }
 }
+
+// exo 2 FOR
+ document.write('<p>*************** EXO 2 ***************</p>');
+
+ var nbrUtilisateur1 = parseInt(prompt('Exo 2 : rentrez un nombre entier, merci '));
+ console.log(typeof nbrUtilisateur1, nbrUtilisateur1);
+
+ var nbrUtilisteurMax = (nbrUtilisateur1 + 10);
+ console.log(typeof nbrUtilisteurMax, nbrUtilisteurMax);
+
+ for (var i = nbrUtilisateur1; i<=nbrUtilisteurMax; i++) {
+    if (i % 2 === 0) {
+       document.write('<p class="stabilo"> ' + i + ' est pair.</p>');
+    } else {
+        document.write('<p> ' + i + ' est impair.</p>');
+    }
+}
+
+// exo 3 WHILE
+document.write('<p>*************** EXO 3 ***************</p>');
